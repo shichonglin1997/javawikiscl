77bc6cf102fbee6a620496f9c6ea0e070071a7b4
shichonglin1997 <shi_chonglin@163.com>
2022-06-18 19:10:43 +0900

    init 2-2

---
+++ src/main/java/com/jiawa/wikiscl/WikisclApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.wikiscl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikisclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikisclApplication.class, args);
+    }
+
+}
