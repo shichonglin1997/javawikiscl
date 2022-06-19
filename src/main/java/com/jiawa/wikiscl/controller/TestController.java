05bb84274387726301cd09c2ca72b212bde06c8c
shichonglin1997 <shi_chonglin@163.com>
2022-06-19 20:59:53 +0900

    init 2-7 test update

--- src/main/java/com/jiawa/wikiscl/controller/TestController.java
+++ src/main/java/com/jiawa/wikiscl/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.jiawa.wikiscl.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -11,5 +14,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "HELLO WORLD";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "HELLO WORLD POST!"+name;
     }
 }
