e957b4616d9d1f6d15f71948ccbf7ea73c677d0a
shichonglin1997 <shi_chonglin@163.com>
2022-06-19 11:55:26 +0900

    init 2-6 hello world

--- src/main/java/com/jiawa/wikiscl/config/WikisclApplication.java
+++ src/main/java/com/jiawa/wikiscl/config/WikisclApplication.java
@@ -1,11 +1,17 @@
-package com.jiawa.wikiscl;
+package com.jiawa.wikiscl.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+
+
+
+
+@ComponentScan("com.jiawa")
 @SpringBootApplication
 public class WikisclApplication {
 
