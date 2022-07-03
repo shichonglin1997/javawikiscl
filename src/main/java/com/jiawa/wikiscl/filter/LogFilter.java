484d029b84524c915bf23f320710d17225bb44ff
shichonglin1997 <shi_chonglin@163.com>
2022-07-03 16:51:42 +0900

    5-7 use filter to print the request interface timing consume

---
+++ src/main/java/com/jiawa/wikiscl/filter/LogFilter.java
@@ -0,0 +1,35 @@
+package com.jiawa.wikiscl.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        //print request message
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter start -------------");
+        LOG.info("request address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("remote address: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter time consuming：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,23 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/* axios interceptor
+*/
+axios.interceptors.request.use(function (config) {
+    console.log('request parameter：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('response result：', response);
+    return response;
+}, error => {
+    console.log('response error：', error);
+    return Promise.reject(error);
+});
+
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
