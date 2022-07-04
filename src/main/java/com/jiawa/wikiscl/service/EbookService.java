5d7a709cc4cfaa431c828c5a8cd65bc755c821f8
shichonglin1997 <shi_chonglin@163.com>
2022-07-04 19:53:33 +0900

    6-4  pagehelper integration

--- src/main/java/com/jiawa/wikiscl/service/EbookService.java
+++ src/main/java/com/jiawa/wikiscl/service/EbookService.java
@@ -1,12 +1,16 @@
 package com.jiawa.wikiscl.service;
 
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wikiscl.domain.Ebook;
 import com.jiawa.wikiscl.domain.EbookExample;
 import com.jiawa.wikiscl.mapper.EbookMapper;
 import com.jiawa.wikiscl.req.EbookSaveReq;
 import com.jiawa.wikiscl.resp.EbookResp;
 import com.jiawa.wikiscl.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -17,17 +21,25 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookSaveReq req){
+
         EbookExample ebookExample = new EbookExample();
         // like a where condition
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
+        // to paginate the first sql statement result under this pagehelper statement (selectByExample in this page)
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("total rows ：{}", pageInfo.getTotal());
+        LOG.info("total pages：{}", pageInfo.getPages());
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
     }
